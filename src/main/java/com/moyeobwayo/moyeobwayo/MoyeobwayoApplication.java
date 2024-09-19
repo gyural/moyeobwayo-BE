afa14f308120f6e2bbd85cf4461cc69536e96eaa
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-09-19 21:24:52 +0900

    initial-commit

---
+++ src/main/java/com/moyeobwayo/moyeobwayo/MoyeobwayoApplication.java
@@ -0,0 +1,13 @@
+package com.moyeobwayo.moyeobwayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeobwayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyeobwayoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moyeobwayo/moyeobwayo/MoyeobwayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyeobwayo.moyeobwayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeobwayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
