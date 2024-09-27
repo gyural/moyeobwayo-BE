67bb72c4ac62eaea9d2320622398411eb0c48f12
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-09-27 11:41:28 +0900

    kakao확정하기 메시지 전송 기능 성공--테스트 코드 X

--- src/main/java/com/moyeobwayo/moyeobwayo/Repository/KakaoProfileRepository.java
+++ src/main/java/com/moyeobwayo/moyeobwayo/Repository/KakaoProfileRepository.java
@@ -3,5 +3,5 @@ package com.moyeobwayo.moyeobwayo.Repository;
 import com.moyeobwayo.moyeobwayo.Domain.KakaoProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KakaoProfileRepository extends JpaRepository<KakaoProfile, Long> {
+public interface KakaoProfileRepository extends JpaRepository<KakaoProfile, Integer> {
 }
