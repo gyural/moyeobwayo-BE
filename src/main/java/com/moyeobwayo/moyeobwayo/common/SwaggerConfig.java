225fd9599285b493de59d66e1ac8c228e82c060a
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-10-01 17:37:44 +0900

    feat: cors-allow세팅+swagger세팅

---
+++ src/main/java/com/moyeobwayo/moyeobwayo/common/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.moyeobwayo.moyeobwayo.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0.0")
+                        .description("This is the API documentation for the project"));
+    }
+}
