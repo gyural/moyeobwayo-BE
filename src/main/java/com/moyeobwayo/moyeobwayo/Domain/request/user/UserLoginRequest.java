3d1a6aed5c92fd07e0348dccdd7122b108beb3ab
Gogumi33 <135415213+Gogumi33@users.noreply.github.com>
2024-10-09 00:06:59 +0900

    loginëautomatically add alarm tuple

--- src/main/java/com/moyeobwayo/moyeobwayo/Domain/request/user/UserLoginRequest.java
+++ src/main/java/com/moyeobwayo/moyeobwayo/Domain/request/user/UserLoginRequest.java
@@ -1,13 +1,19 @@
 package com.moyeobwayo.moyeobwayo.Domain.request.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class UserLoginRequest {
     // DTO
     private String userName;
     private String password;
     private int partyId;  // 로그인 시 파티 ID도 함께 전달
+
+    @JsonProperty("isKakao")
+    private boolean isKakao;  // 카카오 유저인지 여부 추가
 }
