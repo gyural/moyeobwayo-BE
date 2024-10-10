39e9533851aaa028b8663b3b997b9aa0a6ed8cc1
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-10-10 20:56:45 +0900

    feat: 배포를 위한 docker-file수정 및 cors-allow재설정

--- src/main/java/com/moyeobwayo/moyeobwayo/CorsConfig.java
+++ src/main/java/com/moyeobwayo/moyeobwayo/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 허용할 출처
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 쿠키를 포함한 요청을 허용할지 여부
